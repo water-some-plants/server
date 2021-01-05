af7384cdf92c95664233d04f197a43e245a1d1ac
CarlosColindres <70364536+CarlosColindres@users.noreply.github.com>
2021-01-04 20:34:06 -0500

    Update auth-router.js

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const userDb = require('./user-modal')
+const bcryptjs = require('bcryptjs')
 
 //middleware
 const middleware = require('../middleware/middleware')
@@ -9,30 +11,49 @@ const makeToken = require('../helpers/make-token')
 
 const router = express.Router()
 
-router.post('/login', middleware.loginValidation, (req, res) => {
+router.post('/login', middleware.loginValidation, async (req, res) => {
+  const { username, password } = req.body
   try {
-    const token = makeToken({
-      id: 5,
-      username: req.body.username,
-      password: req.body.password,
-    })
-
-    res.status(200).json({ token })
+    const [user] = await userDb.findBy({ username })
+    if (user && bcryptjs.compareSync(password, user.password)) {
+      const token = makeToken({ id: user.id, username })
+      res.status(200).json({ token })
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' })
+    }
   } catch (error) {
     res.status(401).json({ message: 'Invalid credentials' })
   }
 })
 
-router.post('/register', middleware.registerValidation, (req, res) => {
+router.post('/register', middleware.registerValidation, async (req, res) => {
   const newUser = {
     username: req.body.username,
-    phoneNumber: req.body.phoneNumber,
+    phone_number: req.body.phoneNumber,
     password: hashPassword(req.body.password),
   }
   try {
-    res.status(201).json(newUser)
+    const user = await userDb.add(newUser)
+    if (user.id) {
+      res.status(201).json({ username: user.username, id: user.id })
+    } else {
+      res.status(500).json('username is taken')
+    }
   } catch (error) {
     res.status(500).json('username is taken')
+  }
+})
+
+router.put('/user/:id', async (req, res) => {
+  const updateUser = {
+    username: req.body.username,
+    phone_number: req.body.phoneNumber
+  }
+  try {
+    const updatedUser = await userDb.update(req.params.id, updateUser)
+    res.status(201).json(updatedUser)
+  } catch (error) {
+    res.status(500).json({message: 'username taken'})
   }
 })
 
